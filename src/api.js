35a70430d213ad51ed463a70ff4a44a076a99e87
monicacholico <monicachol@hotmail.com>
2018-04-25 10:46:09 -0500

    "working on function that adds movies to the json"

--- src/api.js
+++ src/api.js
@@ -2,5 +2,17 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
-  }
+  },
+    addMovies: () => {
+        //return fetch('./api/movies')
+        const theMovies = '/api/movies';
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(movies),
+        };
+        fetch(theMovies, options)
+            .then(/* movie was created successfully */)
+            .catch(/* handle errors */);
+    }
 };
+
