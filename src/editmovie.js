fb4b313ce4d6292ef0f87a24dcbc89f88ba2ceb7
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-25 16:31:25 -0500

    added edit button

---
+++ src/editmovie.js
@@ -0,0 +1,3 @@
+module.exports = (title, rating) => {
+    return `<input type="text" placeholder="New Title"><input type="text" placeholder="New Rating"><button>Submit</button>`
+};
--- src/index.js
+++ src/index.js
@@ -9,13 +9,15 @@
  */
 const {getMovies, addMovies, deleteMovies} = require('./api.js');
 const $ = require('jquery');
-const movieDisplayer = require('./displayer.js')
+const movieDisplayer = require('./displayer.js');
+const editMovie = require('./editmovie.js');
 
 getMovies().then((movies) => {
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
         console.log(`id#${id} - ${title} - rating: ${rating}`);
-        document.querySelector('.additionalMovie').innerHTML += `<h1> ${title} - rating: ${rating}<button data-id="${id}" class="deletebutton">Delete</button></h1> `;
+        document.querySelector('.additionalMovie').innerHTML +=
+            `<h1> ${title} - rating: ${rating}<button data-id="${id}" class="deletebutton">Delete</button><button class="editbutton">Edit</button></h1> `;
         $('.container').toggleClass("container");
         $('.loadimg').hide();
     });
@@ -43,6 +45,15 @@ $('.additionalMovie').on('click', '.deletebutton', (e)=>{
     console.log($(e.target).data('id'));
     $(e.target).parent('h1').remove();
     deleteMovies($(e.target).data('id'));
+
+
+});
+
+$('.additionalMovie').on('click', '.editbutton', (e)=>{
+    console.log('hello');
+    e.preventDefault();
+    editMovie($(e.target).data('id'));
+
 
 
 });
