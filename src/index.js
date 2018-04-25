ac9e3f74dbc8cf257a972cbe169faffad3f5ebe0
monicacholico <monicachol@hotmail.com>
2018-04-25 10:46:09 -0500

    "working on function that adds movies to the json"

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const $ = require('jquery');
+
+
+const {getMovies, addMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -18,3 +21,23 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+const movieDisplayer = require('./displayer.js');
+
+
+
+$("#add-movie").click((e)=>{
+    event.preventDefault();
+
+    const title = $('#title-movie').val();
+    const rating = $('#rating-movie').val();
+
+    const movieRow = movieDisplayer(title, rating);
+    // movies.push({title: "", rating:"" });
+
+    $("tbody").append(movieRow);
+});
+
+
+
