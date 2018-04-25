0e4ea0c5b4b12852f0d6751c01bc2f3a5bc79e23
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-25 09:44:45 -0500

    added loading img before ajax call

--- src/index.js
+++ src/index.js
@@ -13,8 +13,15 @@ getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    document.write(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+
+
+
+
