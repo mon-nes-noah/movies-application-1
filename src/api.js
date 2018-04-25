e70618da040b724c46b3c0cb37e7449666c1c12f
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-25 15:14:48 -0500

    need to have delete button remove from DB

--- src/api.js
+++ src/api.js
@@ -13,7 +13,17 @@ module.exports = {
 
         return fetch(theMovies, options)
             .then(response => response.json());
+    },
+    deleteMovies: (id) => {
+        const theMovies =`/api/movies/${id}`;
+        const options = {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(id)
+        };
+        return fetch(theMovies, options)
+            .then(response => response.json());
     }
-}
+};
 
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 /**
  * require style imports
  */
-const {getMovies, addMovies} = require('./api.js');
+const {getMovies, addMovies, deleteMovies} = require('./api.js');
 const $ = require('jquery');
 const movieDisplayer = require('./displayer.js')
 
@@ -15,7 +15,7 @@ getMovies().then((movies) => {
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
         console.log(`id#${id} - ${title} - rating: ${rating}`);
-        document.querySelector('.additionalMovie').innerHTML += `<h1> ${title} - rating: ${rating} <button class="deletebutton">Delete</button></h1>`;
+        document.querySelector('.additionalMovie').innerHTML += `<h1> ${title} - rating: ${rating}<button data-id="${id}" class="deletebutton">Delete</button></h1> `;
         $('.container').toggleClass("container");
         $('.loadimg').hide();
     });
@@ -38,9 +38,16 @@ $('#add-movie').click((e) => {
 
 });
 
-$('.deletebutton').click((e)=>{
+$('.additionalMovie').on('click', '.deletebutton', (e)=>{
+    console.log("test");
     e.preventDefault();
-})
+    console.log($(e.target).data('id'));
+    $(e.target).parent('h1').remove();
+    deleteMovies();
+
+
+
+});
 
 
 
