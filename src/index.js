7a01c212549a83ef20fc4afa04897c780aed0e5f
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-25 10:53:44 -0500

    merging

--- src/index.js
+++ src/index.js
@@ -8,17 +8,22 @@
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
-    document.write(`id#${id} - ${title} - rating: ${rating}`);
+    document.querySelector('body').innerHTML += `id#${id} - ${title} - rating: ${rating}`;
+    $('.container').toggleClass("container");
+    $('.loadimg').hide();
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+// $('#edit').css('display', 'block');
 
 
 
