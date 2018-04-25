652e11b72d5910189b43879551a53087cb9ba781
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-25 13:36:18 -0500

    added delele button ( unresponsive)

--- src/index.js
+++ src/index.js
@@ -7,25 +7,40 @@
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, addMovies} = require('./api.js');
 const $ = require('jquery');
+const movieDisplayer = require('./displayer.js')
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-    document.querySelector('body').innerHTML += `id#${id} - ${title} - rating: ${rating}`;
-    $('.container').toggleClass("container");
-    $('.loadimg').hide();
-  });
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+        document.querySelector('.additionalMovie').innerHTML += `<h1> ${title} - rating: ${rating} <button class="deletebutton">Delete</button></h1>`;
+        $('.container').toggleClass("container");
+        $('.loadimg').hide();
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
 });
 
-// $('#edit').css('display', 'block');
 
+$('#add-movie').click((e) => {
+    e.preventDefault();
+    console.log('test');
+    const title = $('#title-movie').val();
+    const rating = $('#rating-movie').val();
 
+    addMovies({title, rating});
+
+    const movieRow = movieDisplayer(title, rating);
+    $("tbody").append(movieRow)
+
+});
+
+$('.deletebutton').click((e)=>{
+    e.preventDefault();
+})
 
 
 
